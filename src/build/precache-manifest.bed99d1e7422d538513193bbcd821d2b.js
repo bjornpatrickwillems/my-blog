1468de6f9a7cc8fa51786405cf8a165c3993b9e3
Bjorn <bjorn.patrick.willems@gmail.com>
2020-01-09 23:40:03 +0100

    First commit

---
+++ src/build/precache-manifest.bed99d1e7422d538513193bbcd821d2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cccd3b9db61d5babd85dd0797d21bd71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccb6886eb5e05d1e5d77",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "1f620f27741a79b7ea59",
+    "url": "/static/js/2.06c92547.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.06c92547.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ccb6886eb5e05d1e5d77",
+    "url": "/static/js/main.ae9b94eb.chunk.js"
+  },
+  {
+    "revision": "c59d8ddbc435ff2fc636",
+    "url": "/static/js/runtime-main.2bac53c8.js"
+  }
+]);
